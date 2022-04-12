65f9a1401263b899cd27a2a3cbfe87f1c7200ff3
Jeetu <jeetuvakode@gmail.com>
2022-04-12 20:50:45 +0530

    commites changes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -16,7 +18,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://jeetu:Pj0wrf9FBckXQlzy@cluster0.6oqim.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
